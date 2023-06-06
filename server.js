800b824c75fefbf8abacdd145bb6ffd2debfbdf2
Arjun Vijayan <vijayanarjun4@gmail.com>
2023-06-06 21:19:03 +0530

    connection from ReactJS

--- server.js
+++ server.js
@@ -2,29 +2,22 @@ const User = require("./model/user")
 const mongoose = require("mongoose")
 
 const express = require("express")
-
-
 const app = express()
-const url = 'mongodb+srv://arjun:arjun@workshop.1les3e8.mongodb.net/Workshop?retryWrites=true&w=majority'
 
+app.post("/post", (req, res) => {
+    console.log("Connected to React");
+    res.redirect("/");
+  });
+
+const url = 'mongodb+srv://arjun:arjun@workshop.1les3e8.mongodb.net/Workshop?retryWrites=true&w=majority'
 const connect= async()=>{
     try{
         await mongoose.connect(url);
-        const Users = new User({
-            Name: 'Aswin P',
-            Email: 'vijayanarjun@gmail.com',
-            Make: 'Volvo',
-            Model: 'XC90'
-        })
-        
-        Users.save();
-        
         const firstUser = await User.findOne({});
         console.log(firstUser)
     }catch(err){
         console.log(err);
     }  
 }
-
 connect();
-
+app.listen(8080, console.log("Server Started at 8080"))
