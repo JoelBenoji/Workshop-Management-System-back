1ce7ba35d8f3420ee9900b11e4783900ac527070
Arjun Vijayan <vijayanarjun4@gmail.com>
2023-06-07 00:26:53 +0530

    User registration Setup

--- server.js
+++ server.js
@@ -1,18 +1,52 @@
 const User = require("./model/user")
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+var cors = require('cors')
+var events = require('events')
+var eventEmitter = new events.EventEmitter();
 
 const express = require("express")
 const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(bodyParser.urlencoded(
+    {
+        extended:true
+    }
+));
 
-app.post("/post", (req, res) => {
-    console.log("Connected to React");
-    res.redirect("/");
-  });
+var info = {};
+
+//Post Request Handling
+app.post("/", (req, res) => {
+    response = {
+        name: req.body.name,
+        email: req.body.email,
+        make: req.body.make,
+        model: req.body.model
+    };
+    info = response
+    console.log(response)
+    res.send("Registered")
+    eventEmitter.emit('send')
+});
 
 const url = 'mongodb+srv://arjun:arjun@workshop.1les3e8.mongodb.net/Workshop?retryWrites=true&w=majority'
+var handler = function(){
+    //Send to Database
+    const Users = new User({
+        Name: info.name,
+        Email: info.email,
+        Make: info.make,
+        Model: info.model,
+    })
+    Users.save();
+}
 const connect= async()=>{
     try{
-        await mongoose.connect(url);
+        //Connect to Database
+        mongoose.connect(url);
+        eventEmitter.on('send',handler);
         const firstUser = await User.findOne({});
         console.log(firstUser)
     }catch(err){
@@ -20,4 +54,5 @@ const connect= async()=>{
     }  
 }
 connect();
+
 app.listen(8080, console.log("Server Started at 8080"))
