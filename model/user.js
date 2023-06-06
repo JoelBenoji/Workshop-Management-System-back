d6ca8f296e7036f5538122665982490158096efb
Arjun Vijayan <vijayanarjun4@gmail.com>
2023-06-06 21:04:49 +0530

    user schema setup

---
+++ model/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const  {Schema, model} = mongoose
+
+
+const UserSchema = new Schema({
+    Name: String,
+    Email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    Make: String,
+    Model: String
+});
+
+module.exports = mongoose.model('User', UserSchema)
--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-const express = require("express")
+const User = require("./model/user")
 const mongoose = require("mongoose")
 
-const app = express()
-const url = 'mongodb+srv://arjun:arjun@workshop.1les3e8.mongodb.net/?retryWrites=true&w=majority'
+const express = require("express")
 
-const connect = async()=>{
+
+const app = express()
+const url = 'mongodb+srv://arjun:arjun@workshop.1les3e8.mongodb.net/Workshop?retryWrites=true&w=majority'
+
+const connect= async()=>{
     try{
         await mongoose.connect(url);
-        console.log("Connected to Mongoose")
+        const Users = new User({
+            Name: 'Aswin P',
+            Email: 'vijayanarjun@gmail.com',
+            Make: 'Volvo',
+            Model: 'XC90'
+        })
+        
+        Users.save();
+        
+        const firstUser = await User.findOne({});
+        console.log(firstUser)
     }catch(err){
         console.log(err);
-    }
+    }  
 }
 
 connect();
 
-app.listen(4500,()=>{
-    console.log("Success");
-})
