12bf6b94a4e5c07682e82abfe9909f3b18b71094
Arjun Vijayan <vijayanarjun4@gmail.com>
2023-07-16 18:28:21 +0530

    Employee Job List Setup

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 var ObjectID = require('mongodb').ObjectID;
 
-const {MongoClient} = require('mongodb')
+const { MongoClient } = require('mongodb')
 
 var cors = require('cors')
 var events = require('events')
@@ -17,29 +17,29 @@ app.use(express.json())
 app.use(cors())
 app.use(bodyParser.urlencoded(
     {
-        extended:true
+        extended: true
     }
 ));
 
 var info = {};
 //Get Request Handling(Signup)
-app.get("/signup/make",async(req, res)=>{
+app.get("/signup/make", async (req, res) => {
     var cars = []
-    try{
+    try {
         await client.connect();
-        const data = await client.db("Workshop").collection("Cars").find({},{Make:1}).toArray();
-        for(var i=0;i < data.length;i++){
-            if(cars.includes(data[i].Make)){
+        const data = await client.db("Workshop").collection("Cars").find({}, { Make: 1 }).toArray();
+        for (var i = 0; i < data.length; i++) {
+            if (cars.includes(data[i].Make)) {
                 continue;
             }
-            else{
+            else {
                 await cars.push(data[i].Make)
                 cars.sort();
             }
         }
-    }catch(e){
-        console.log(e); 
-    }finally{
+    } catch (e) {
+        console.log(e);
+    } finally {
         await client.close();
     }
     res.json({
@@ -59,229 +59,297 @@ app.post("/signup", (req, res) => {
     };
     info = response
     const url = 'mongodb+srv://arjun:arjun@workshop.1les3e8.mongodb.net/Workshop?retryWrites=true&w=majority'
-    const connect= async()=>{
-    try{
-        //Connect to Database
-        mongoose.connect(url);
-        var Users = new User({
-            Name: info.name,
-            Email: info.email,
-            Password: info.password,
-            Make: info.make,
-            Model: info.model,
-        })
-        const result = await User.findOne({Email: info.email})
+    const connect = async () => {
+        try {
+            //Connect to Database
+            mongoose.connect(url);
+            var Users = new User({
+                Name: info.name,
+                Email: info.email,
+                Password: info.password,
+                Make: info.make,
+                Model: info.model,
+            })
+            const result = await User.findOne({ Email: info.email })
 
-        if(!result){
-            Users.save()
-            res.json({"Status":"Success"})
+            if (!result) {
+                Users.save()
+                res.json({ "Status": "Success" })
+            }
+            else {
+                res.json({ "Status": "Failure" })
+            }
+        } catch (err) {
+            console.log(err);
         }
-        else{
-            res.json({"Status":"Failure"})
-        }
-    }catch(err){
-        console.log(err);
-    }  
-}
+    }
     connect();
 });
 
-app.post("/signup/model",async(req, res)=>{
-    var models= []
+app.post("/signup/model", async (req, res) => {
+    var models = []
     response = {
         Make: req.body.Make
     }
-    try{
+    try {
         await client.connect();
-        const data = await client.db("Workshop").collection("Cars").find({Make: response.Make }).toArray();
-        for(var i=0;i < data.length;i++){
-            if(models.includes(data[i].Model)){
+        const data = await client.db("Workshop").collection("Cars").find({ Make: response.Make }).toArray();
+        for (var i = 0; i < data.length; i++) {
+            if (models.includes(data[i].Model)) {
                 continue;
             }
-            else{
+            else {
                 await models.push(data[i].Model)
                 models.sort();
             }
         }
-        if(models !== []){
+        if (models !== []) {
             res.json({
                 Models: models
             })
         }
-    }catch(e){
-        console.log(e); 
+    } catch (e) {
+        console.log(e);
     }
 })
-
-//Post Request Handling(Login User)
-app.post("/",async(req,res)=>{
-    response = {
-        Email : req.body.email,
-        Password: req.body.password
-    }
-    const checksend=async(log)=>{
-        if(log.Password === req.body.password){
-                console.log("User Login Success")
-                await res.json({"Success": "true",
-                "Name": log.Name,
-                "Make": log.Make,
-                "Model": log.Model,
-                "Email": response.email,
-            })
-                applist(response.email)
-            }
-        else{
-                console.log("Failed")
-            }
-    }
-    const connect=async()=>{
-        try{
-            const url = 'mongodb+srv://arjun:arjun@workshop.1les3e8.mongodb.net/Workshop?retryWrites=true&w=majority'
-            await mongoose.connect(url)
-            const log =await User.findOne({Email: req.body.email})
-            await checksend(log);
-        }catch(err){
-            console.log(err)
-            await res.json({"Success":"false"})
-        }
-    }
-    connect();
-})
-
 
 
 //Appointment List function User
-const applist=(email)=>{
-    app.get('/user/dashboard',async(req,res)=>{
-        var appointments = []
-        const listget=async()=>{
-            try{
-                await client.connect()
-                const data = await client.db("Workshop").collection("Appointments").find({
-                    Email : email
-                }).toArray()
-                console.log(data)
-                appointments = data
-                await res.json(appointments)
-            }
-            catch(e){
-                console.log(e)
-            }
-        }
-        listget()
-    })
-}
-
-
-//New Appointment
-app.post("/user/dashboard",async(req,res)=>{
+app.post('/user/dashboard', async (req, res) => {
+    var appointments = []
     response = {
-        Name: req.body.name,
-        Email: req.body.email,
-        Category : req.body.category,
-        Date: req.body.date,
-        Description: req.body.desc,
-        Make: req.body.make,
-        Model:req.body.model,
+        Email: req.body.email
     }
-    console.log(response)
-    const connect=async()=>{
-        try{
-            await client.connect();
-            const data = await client.db("Workshop").collection("Appointments").insertOne({
-                Name: response.Name,
-                Email: response.Email,
-                Date: response.Date,
-                Make: response.Make,
-                Model: response.Model,
-                Category : response.Category,
-                Description: response.Description,
-                Status: 'pending',
-                Empid: null,
+    try {
+        await client.connect()
+        var data = await client.db("Workshop").collection("Appointments").find({
+            Email: response.Email
+        }).toArray()
+    }
+    catch (e) {
+        console.log(e)
+    }
+    appointments = data
+    res.json(appointments)
+})
+
+//Post Request Handling(Login User)
+app.post("/", (req, res) => {
+    response = {
+        Email: req.body.email,
+        Password: req.body.password
+    }
+    const checksend = async (log) => {
+        if (log.Password === req.body.password) {
+            console.log("User Login Success")
+            res.json({
+                "Success": "true",
+                "Name": log.Name,
+                "Make": log.Make,
+                "Model": log.Model,
+                "Email": response.Email,
             })
-        }catch(err){
+        }
+        else {
+            console.log("Failed")
+        }
+    }
+    const connect = async () => {
+        try {
+            const url = 'mongodb+srv://arjun:arjun@workshop.1les3e8.mongodb.net/Workshop?retryWrites=true&w=majority'
+            await mongoose.connect(url)
+            const log = await User.findOne({ Email: req.body.email })
+            await checksend(log);
+        } catch (err) {
             console.log(err)
-            await res.json({"Success":"false"})
+            await res.json({ "Success": "false" })
         }
     }
     connect();
-    applist(response.email)
+})
+
+//New Appointment
+app.post("/user/dashboard/newappoint", async (req, res) => {
+    response = {
+        Name: req.body.name,
+        Email: req.body.email,
+        Category: req.body.category,
+        Date: req.body.date,
+        Description: req.body.desc,
+        Make: req.body.make,
+        Model: req.body.model,
+    }
+    const connect = async () => {
+        try {
+            var flag = true
+            await client.connect();
+            const check = await client.db('Workshop').collection('Appointments').find({
+                Email: response.Email
+            }).toArray()
+            if (check === []) {
+                flag = true
+            }
+            else {
+                for (i = 0; i < check.length; i++) {
+                    if (check[i].status === 'Finished') {
+                        flag = true
+                    }
+                    else {
+                        flag = false
+                        break;
+                    }
+                }
+            }
+            if (flag === true) {
+                const data = await client.db("Workshop").collection("Appointments").insertOne({
+                    Name: response.Name,
+                    Email: response.Email,
+                    Date: response.Date,
+                    Make: response.Make,
+                    Model: response.Model,
+                    Category: response.Category,
+                    Description: response.Description,
+                    Status: 'Pending',
+                    Empid: null,
+                })
+                await res.json({
+                    "Status": "Inserted Successfully"
+                })
+            }
+            else {
+                await res.json({
+                    "Status": "Pending Appointments to be completed"
+                })
+                console.log("On submit mail" + response.Email)
+            }
+        } catch (err) {
+            console.log(err)
+            await res.json({ "Status": "Some error occured" })
+        }
+    }
+    connect();
 })
 
 // Post Request Handling (Employee Login)
-app.post("/emplogin",(req,res)=>{
+app.post("/emplogin", (req, res) => {
     response = {
-        Empid : req.body.Empid,
-        Password : req.body.Password
+        Empid: req.body.Empid,
+        Password: req.body.Password
     }
-    const main =async()=>{
-        try{
+    const main = async () => {
+        try {
             await client.connect();
-            const data = await client.db("Workshop").collection("Employee").findOne({Empid : response.Empid});
-            if(data.Password == response.Password){
-                await res.json({"Success": "true",
-                "Name": data.Name,
-                "Phone": data.Phone,
-                "Category": data.Category,
+            const data = await client.db("Workshop").collection("Employee").findOne({ Empid: response.Empid });
+            if (data.Password == response.Password) {
+                await res.json({
+                    "Success": "true",
+                    "Name": data.Name,
+                    "Phone": data.Phone,
+                    "Category": data.Category,
                 })
-                await applist(data.Category)
                 console.log("Employee Login Success")
             }
-        }catch(e){
-            console.log(e); 
+        } catch (e) {
+            console.log(e);
         }
     }
     main();
 })
 
 //Appointment View Employee
-app.get('/emp/dashboard/Electrical',async(req,res)=>{
-    try{
+app.get('/emp/dashboard/Electrical', async (req, res) => {
+    try {
         await client.connect()
         const data = await client.db("Workshop").collection("Appointments").find({
-            Status: "pending",
+            Status: "Pending",
             Category: 'Electrical'
         }).toArray()
         await res.json(data)
-        console.log(data)
-    }catch(e){
+    } catch (e) {
         console.log(e)
     }
 })
-app.get('/emp/dashboard/General',async(req,res)=>{
-    try{
+app.get('/emp/dashboard/General', async (req, res) => {
+    try {
         await client.connect()
         const data = await client.db("Workshop").collection("Appointments").find({
-            Status: "pending",
+            Status: "Pending",
             Category: 'General'
         }).toArray()
         await res.json(data)
-        console.log(data)
-    }catch(e){
+    } catch (e) {
         console.log(e)
     }
 })
-app.post('/emp/dashboard',async(req,res)=>{
-    
+app.get('/emp/dashboard/Body Work', async (req, res) => {
+    try {
+        await client.connect()
+        const data = await client.db("Workshop").collection("Appointments").find({
+            Status: "Pending",
+            Category: 'Body Work'
+        }).toArray()
+        await res.json(data)
+    } catch (e) {
+        console.log(e)
+    }
+})
+app.get('/emp/dashboard/Air Conditioning', async (req, res) => {
+    try {
+        await client.connect()
+        const data = await client.db("Workshop").collection("Appointments").find({
+            Status: "Pending",
+            Category: 'Air Conditioning'
+        }).toArray()
+        await res.json(data)
+    } catch (e) {
+        console.log(e)
+    }
+})
+app.get('/emp/dashboard/Engine Specialist', async (req, res) => {
+    try {
+        await client.connect()
+        const data = await client.db("Workshop").collection("Appointments").find({
+            Status: "Pending",
+            Category: 'Engine Specialist'
+        }).toArray()
+        await res.json(data)
+    } catch (e) {
+        console.log(e)
+    }
+})
+app.post('/emp/dashboard/joblist', async (req, res) => {
+    try {
+        await client.connect()
+        const data = await client.db("Workshop").collection("Appointments").find({
+            Status: "Accepted",
+            Empid: req.body.Empid
+        }).toArray()
+        await res.json(data)
+    } catch (e) {
+        console.log(e)
+    }
+})
+app.post('/emp/dashboard', async (req, res) => {
+
     response = {
         id: req.body.id,
         Status: req.body.Status,
         Empid: req.body.Empid,
     }
     console.log(response)
-    try{
+    try {
         await client.connect()
         const data = await client.db('Workshop').collection('Appointments').updateOne({
             _id: new mongoose.Types.ObjectId(response.id)
-        },{
-            $set:{
+        }, {
+            $set: {
                 Status: 'Accepted',
                 Empid: response.Empid
             }
         }
         )
         console.log(data)
-    }catch(e){
+    } catch (e) {
         console.log(e)
     }
 })
